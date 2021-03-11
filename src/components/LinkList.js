eb895c6e50552c1139570f6d67a57160dc1f214e
Lisa Maskovich <lisamariejensenmaskovich@gmail.com>
2021-03-11 11:41:12 -0800

    removing the hardcoded linksToRender

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -17,19 +17,21 @@ const FEED_QUERY = gql`
 `;
 
 const LinkList = () => {
-    const linksToRender = [
-        {
-            id:'1',
-            description:'Prisma gives you a powerful database toolkit',
-            url: 'https://prisma.io'
-        },
-        {
-            id:'2',
-            description: 'The best GraphQL client',
-            url: 'https://www.apollographql.com/docs/react/'
+    const { data } = useQuery(FEED_QUERY);
 
-        }
-    ];
+    return (
+        <div>
+            {data && (
+                <>
+                    {data.feed.links.map((link) => (
+                        <link key={link.id} link={link} />
+                    ))}
+                    </>
+            )}
+        </div>
+        );
+    };
+
 
     return (
         <div>
