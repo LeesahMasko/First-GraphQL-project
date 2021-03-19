6222666ce28af4aff39ee7f4e54212c66854d95d
Lisa Maskovich <lisamariejensenmaskovich@gmail.com>
2021-03-19 10:17:39 -0700

    re-added the mutations that I removed.  Silly me.

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,6 +3,33 @@ import { gql, useMutation } from '@apollo/client';
 import { useHistory } from 'react-router';
 import { AUTH_TOKEN } from '../constants';
 
+const SIGNUP_MUTATION = gql`
+  mutation SignupMutation(
+    $email: String!
+    $password: String!
+    $name: String!
+  ) {
+    signup(
+      email: $email
+      password: $password
+      name: $name
+    ) {
+      token
+    }
+  }
+`;
+
+const LOGIN_MUTATION = gql`
+  mutation LoginMutation(
+    $email: String!
+    $password: String!
+  ) {
+    login(email: $email, password: $password) {
+      token
+    }
+  }
+`;
+
 const Login = () => {
   const history = useHistory();
   const [formState, setFormState] = useState({
