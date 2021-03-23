2f673f3331a987cdfe299ad4139fb70c2bb9b134
Lisa Maskovich <lisamariejensenmaskovich@gmail.com>
2021-03-23 11:43:59 -0700

    preparing some additional react components for eventual up-voting

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { AUTH_TOKEN, LINKS_PER_PAGE } from '../constants';
 
 const Link = (props) => {
     const { link } = props;
+    const authToken = localStorage.getItem(AUTH_TOKEN)
+
+    const take = LINKS_PER_PAGE;
+    const skip = 0;
+    const orderBy = { createdAt: 'desc' };
+
     return (
         <div>
             <div>
